382f3fbda8c228787339a013db62651b02d7d29a
alexc <alexcolwell13@gmail.com>
2023-09-05 13:41:29 -0500

    setup initial prompt

--- index.js
+++ index.js
@@ -15,7 +15,37 @@ db.connect(function (err) {
 });
 
 function prompt() {
-
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'promptMenu',
+                message: 'What would you like to do?',
+                choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee', 'Quit']
+            }]).then((data) => {
+                let answer = data.promptMenu;
+                if (answer === 'View all Departments') {
+                    allDepartments()
+                }
+                else if (answer === 'View all Roles') {
+                    allRoles()
+                }
+                else if (answer === 'View all Employees') {
+                    allEmployees()
+                }
+                else if (answer === 'Add Department') {
+                    addDepartment()
+                }
+                else if (answer === 'Add Role') {
+                    addRole()
+                }
+                else if (answer === 'Add Employee') {
+                    addEmployee()
+                }
+                else if (answer === 'Update Employee') {
+                    updateEmployee()
+                }
+            })
 }
 
 function allDepartments() {
